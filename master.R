041cdf2212716568fcf0403cf44fbe1e4ee57976
amatthews <anthonymatthews29@gmail.com>
2024-06-18 13:53:46 +0200

    update 18/06/24

--- master.R
+++ master.R
@@ -1,4 +1,13 @@
+################################################################################
+# Purpose: master file
+# Author: Anthony Matthews
+# First created: 18/06/2022
+# GitHub repo: https://github.com/tonymatthews/reduce-pp
+################################################################################
+
+# Packages ---------------------------------------------------------------------
 library(here)
+library(haven)
 library(dplyr)
 library(purrr)
 library(tableone)
@@ -19,10 +28,15 @@ library(rlang)
 library(labelled)
 library(berryFunctions)
 library(ggsci)
-library(tictoc)
 library(rsample)
 library(lobstr)
 library(rms)
 library(furrr)
 library(future.apply)
 library(parallel)
+
+# Data -------------------------------------------------------------------------
+dat_main <- readRDS(file.path(here() %>% dirname(), 'cs-transfer/adb_swe.RDS'))
+dat_pdr <- read_sas(file.path(here() %>% dirname(), 'cs-transfer drugs/ut_r_lmed_29075_2022.sas7bdat'))
+
+
